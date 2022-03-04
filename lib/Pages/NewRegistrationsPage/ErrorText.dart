4b53e616890ab08b40997e70b9ff67dd9cd122e4
mateusz.chabinski <maxteusz@gmail.com>
2022-03-04 11:45:13 +0100

    03.04

---
+++ lib/Pages/NewRegistrationsPage/ErrorText.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ErrorText extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration (
+        borderRadius: BorderRadius.circular(4),
+           border: Border (
+
+           )
+      ),
+      child: Text("Fdfdd"),
+    );
+  }
+}
--- lib/Pages/NewRegistrationsPage/NewRegistrationsPage.dart
+++ lib/Pages/NewRegistrationsPage/NewRegistrationsPage.dart
@@ -4,10 +4,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:http/http.dart' as http;
+import 'package:untitled2/Pages/NewRegistrationsPage/ErrorText.dart';
 import 'dart:developer';
 
 import '../../Objects/Registration.dart';
 import '../../Objects/User.dart';
+import 'AddPeopleDropDownButton.dart';
+import 'DropDownButton.dart';
 
 Registration? registration = null;
 
@@ -100,8 +103,9 @@ class _NewRegistrationPage extends State<NewRegistrationPage> {
                       if (snapshot.hasData) {
                         return AddPeopleDropDownButton(snapshot.data!, "Dodaj użytkownika");
                       } else
-                        return Text('${snapshot.error}');
-                    })),
+                        return  ErrorText();
+                    })
+            ),
             Container(
               margin: EdgeInsets.only(bottom: 10),
               child: MaterialButton(
@@ -122,175 +126,5 @@ class _NewRegistrationPage extends State<NewRegistrationPage> {
   }
 }
 
-class DropDownButton extends StatefulWidget {
-  String label;
-  List<User> users = [];
 
-  DropDownButton(this.users, this.label);
 
-  @override
-  State<StatefulWidget> createState() {
-    return DropDownButtonState(users, label);
-  }
-}
-
-class DropDownButtonState extends State<DropDownButton> {
-  String label;
-  User? selectedValue = null;
-  List<User> users = [];
-
-  DropDownButtonState(this.users, this.label);
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButtonHideUnderline(
-      child: DropdownButton2(
-        isExpanded: true,
-        hint: Text(label),
-        items: users
-            .map((item) => DropdownMenuItem<User>(
-                  value: item,
-                  child: Text(
-                    item.getName(),
-                  ),
-                ))
-            .toList(),
-        value: selectedValue,
-        onChanged: (value) {
-          setState(() {
-            selectedValue = value as User;
-          });
-        },
-        icon: const Icon(
-          Icons.arrow_forward_ios_outlined,
-        ),
-        iconSize: 14,
-        iconEnabledColor: Colors.black,
-        iconDisabledColor: Colors.grey,
-        buttonHeight: 60,
-        buttonPadding: const EdgeInsets.only(left: 14, right: 14),
-        buttonDecoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(4),
-          border: Border.all(
-            color: Colors.grey,
-          ),
-        ),
-        itemHeight: 60,
-        itemPadding: const EdgeInsets.only(left: 14, right: 14),
-        dropdownMaxHeight: 200,
-        dropdownPadding: null,
-        dropdownElevation: 8,
-        scrollbarThickness: 6,
-        scrollbarAlwaysShow: true,
-      ),
-    );
-  }
-}
-
-class AddPeopleDropDownButton extends StatefulWidget {
-
-  List<User> users = [];
-  String label;
-
-  AddPeopleDropDownButton(this.users, this.label);
-
-  @override
-  State<StatefulWidget> createState() {
-    return AddPeopleDropDownButtonState(users, label);
-  }
-}
-
-class AddPeopleDropDownButtonState extends State<AddPeopleDropDownButton> {
-  String label;
-  User? selectedValue = null;
-  List<User> users = [];
-  List<User> attachedUsers = [];
-
-  AddPeopleDropDownButtonState(this.users, this.label);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        DropdownButtonHideUnderline(
-          child: DropdownButton2(
-            isExpanded: true,
-            hint: Text(label),
-            items: users
-                .map((user) => DropdownMenuItem<User>(
-                      value: user,
-                      child: Text(
-                        user.getName(),
-                      ),
-                    ))
-                .toList(),
-            value: selectedValue,
-            onChanged: (value) {
-              setState(() {
-                selectedValue = value as User;
-                if(!attachedUsers.contains(value))
-                attachedUsers.add(value);
-                selectedValue = null;
-              });
-            },
-            icon: const Icon(
-              Icons.arrow_forward_ios_outlined,
-            ),
-            iconSize: 14,
-            iconEnabledColor: Colors.black,
-            iconDisabledColor: Colors.grey,
-            buttonHeight: 60,
-            buttonPadding: const EdgeInsets.only(left: 14, right: 14),
-            buttonDecoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(4),
-              border: Border.all(
-                color: Colors.grey,
-              ),
-            ),
-            itemHeight: 40,
-            itemPadding: const EdgeInsets.only(left: 14, right: 14),
-            dropdownMaxHeight: 200,
-            dropdownPadding: null,
-            dropdownElevation: 8,
-            scrollbarThickness: 6,
-            scrollbarAlwaysShow: true,
-          ),
-        ),
-        Container(
-          height: 150,
-          child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: attachedUsers.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.blue[50],
-                  ),
-
-                  margin: EdgeInsets.only(left: 10, top: 10, right: 10),
-                  child: Row(
-                    children: [
-                      Container(
-                          margin: EdgeInsets.only(left: 10,top: 10, bottom: 10),
-                          child: Text(attachedUsers[index].getName())),
-                      Container(
-                          margin: EdgeInsets.only(left: 10),
-                          child: InkWell(
-                            onTap: () {
-                              setState(() {
-                                attachedUsers.removeAt(index);
-                              });
-                            },
-                            child: Icon(Icons.remove_circle_outline),
-                          )),
-                    ],
-                  ),
-                );
-              }),
-        ),
-      ],
-    );
-  }
-}
